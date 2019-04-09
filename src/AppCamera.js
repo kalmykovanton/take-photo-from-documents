017165ef04877b5d06f3f54f4533705a12c00335
Anton Kalmykov <a.kalmykov@mobidev.biz>
2019-04-09 16:53:58 +0300

    add component

---
+++ src/AppCamera.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
+
+class AppCamera extends Component {
+  render() {
+    const { firstPhoto } = this.props;
+
+    return (
+      <div style={{ visibility: firstPhoto ? "hidden" : "visible" }}>
+        <Camera onTakePhoto={this.props.onTakePhoto} />
+      </div>
+    );
+  }
+}
+
+export default AppCamera;
