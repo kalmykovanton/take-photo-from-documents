762c1d5755cf617e1aabc6ffb392cb98bbb15b3a
Anton Kalmykov <a.kalmykov@mobidev.biz>
2019-04-11 13:02:40 +0300

    change facing mode

--- src/AppCamera.js
+++ src/AppCamera.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Camera from "react-html5-camera-photo";
+import Camera, { FACING_MODES } from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 
 const AppCamera = ({ onTakePhoto, onCameraStart }) => (
   <div>
     <canvas className="canvas-overlay" id="canvas-overlay" />
-    <Camera onTakePhoto={onTakePhoto} onCameraStart={onCameraStart} />
+    <Camera
+      onTakePhoto={onTakePhoto}
+      onCameraStart={onCameraStart}
+      idealFacingMode={FACING_MODES.ENVIRONMENT}
+    />
   </div>
 );
 
